8157a0441f6207c332836d91fd8390db1f4415c2
InvQ <abdulah.alrifai@outlook.com>
2023-04-18 17:53:13 -0600

    Updated var.py and added new file called 'int.py'

--- variables/var.py
+++ variables/var.py
@@ -1,4 +1,11 @@
 # Variables.
 
-name = 'InvQ'
-print('My name is %s' % name)
+first_name = 'InvQ'
+last_name = 'ertial'
+full_name = first_name + " " + last_name
+#print('My name is %s' % name)
+#print('Hello, my name is ' + name)
+print('Hello, my name is ' + full_name)
+
+print(type(full_name))
+
