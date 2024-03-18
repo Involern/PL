c836eb59886005d628d386f5ed0b484c643f4eb7
Involern <abdulah.alrifai@outlook.com>
2024-03-17 20:33:25 -0600

    STRINGMETHODS.py

---
+++ 2024/STRINGMETHODS.py
@@ -0,0 +1,13 @@
+name = "name"
+
+# print(name)
+# print(len(name))
+# print(name.find("n"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count('a'))
+# print(name.replace("a", "o"))
+print(name*3)
