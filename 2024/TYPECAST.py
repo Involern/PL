f41a0a04b82c396f9cae5c635ed0c4040c596846
Involern <abdulah.alrifai@outlook.com>
2024-03-17 20:38:36 -0600

    TYPECAST.py

---
+++ 2024/TYPECAST.py
@@ -0,0 +1,17 @@
+# type casting = convert the data type of a value to another data type.
+
+x = 1
+y = 2.0
+z = "3"
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+# print(x)
+# print(int(y))
+# print(z*3)
+
+print("X IS: " + str(x))
+print("Y IS: " + str(y))
+print("Z IS: " + str(z))
