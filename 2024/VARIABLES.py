1e555969ab4db5dca65624e68b59e6f6eb51a7fe
Involern <abdulah.alrifai@outlook.com>
2024-03-17 20:26:06 -0600

    VARIABLES.py

---
+++ 2024/VARIABLES.py
@@ -0,0 +1,39 @@
+# variable = a container for a value.
+
+# String variable
+name = "string"
+print(type(name))
+
+# if (type(name) == str):
+#     print("This is a string")
+
+first_name = "first"
+last_name = "last"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# int variable
+
+age = 88
+age += 1
+print (type(age))
+
+print(str(age) + full_name)
+
+
+# float datatype
+
+height = 250.5 
+print("Height is: " + str(height)+"cm")
+#print(type(height))
+
+
+# boolean datatype
+
+SWITCH = False
+print("Is switch on: " + str(SWITCH))
+# print(type(SWITCH))
+
+
+
