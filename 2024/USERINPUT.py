dea0b35a4ab9add096876d88792da3316e48860c
Involern <abdulah.alrifai@outlook.com>
2024-03-23 17:13:44 -0600

    USERINPUT.py

---
+++ 2024/USERINPUT.py
@@ -0,0 +1,15 @@
+# INP = input("Enter Password: ")
+
+# if INP == "password":
+#     print("Welcome master")
+
+
+name = input("What is your name?: ")
+age = int(input("What is yoru age?: "))
+height = float(input("How tall are you?: "))
+
+# age = age + 1
+print("Hello "+name)
+print("You are %s years old" % age)
+print("You are "+str(height)+"cm tall")
+
